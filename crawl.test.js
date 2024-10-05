2343a4cba9eeff3cef5a4d079d5c9f40f56a9e01
Christian Gleitze <connectingdotscoding@gmail.com>
2024-10-05 21:33:12 +0200

    add jsdom, expand functionality

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
 test('normalizeURL strip protocol', () => {
@@ -19,5 +19,21 @@ test('normalizeURL strip http', () => {
   const input = 'http://developer.mozilla.org'
   const actual = normalizeURL(input)
   const expected = 'developer.mozilla.org'
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML', () => {
+  const inputHTMLBody = `
+<html>
+  <body>
+    <a href="http://developer.mozilla.org">
+      MDN
+    </a>
+  </body>
+</html>
+`
+  const inputBaseURL = "https://developer.mozilla.org"
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+  const expected = ["developer.mozilla.org"]
   expect(actual).toEqual(expected)
 })
